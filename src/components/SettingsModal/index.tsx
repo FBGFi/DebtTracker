cc8112cb99cb7d8f23f68508d1c493b1f8674151
Aleksi Alhola <aleksi.alhola@student.lab.fi>
2022-05-24 18:57:00 +0300

    added settings input logic

--- src/components/SettingsModal/index.tsx
+++ src/components/SettingsModal/index.tsx
@@ -10,6 +10,57 @@ interface SettingsModalProps extends ReactComponentProps {
     setModal: (modal: null) => void;
 }
 
+const removeSpaces = (value: string) => value.replace(/ /g, "");
+
+const validateBankAccount = (bankAccount: string) => {
+    const characters = removeSpaces(bankAccount).split("");
+    if (characters.length !== 18) return false;
+    for (let i = 2; i < characters.length; i++) {
+        if (isNaN(parseInt(characters[i]))) return false;
+    }
+    return isNaN(parseInt(bankAccount[0])) && isNaN(parseInt(bankAccount[1]));
+}
+
+const validateMobilePay = (mobilePay: string) => {
+    return !isNaN(parseInt(removeSpaces(mobilePay))) && parseInt(removeSpaces(mobilePay)).toString().length === 9;
+}
+
+const splitToChunks = (value: string, chunkSize: number) => value.split("").reduce((resultArray: string[][], item: string, index: number) => {
+    const chunkIndex = Math.floor(index / chunkSize);
+
+    if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = []; // start a new chunk
+    }
+
+    resultArray[chunkIndex].push(item);
+
+    return resultArray;
+}, []).map(chunk => {
+    return chunk.join("");
+});
+
+const parseMobilePay = (mobilePay: string) => {
+    let formattedMobilePay = splitToChunks(removeSpaces(mobilePay), 3);
+    for(let i = formattedMobilePay.length - 1; i > 2; i--) {
+        formattedMobilePay[2] = [...formattedMobilePay[2], ...formattedMobilePay[i]].join("");
+        formattedMobilePay.pop();
+    }
+    return formattedMobilePay.join(" ");    
+}
+
+const parseUserName = (userName: string) => {
+    let formattedUserName: string | string[] = userName.toLowerCase();
+    formattedUserName = formattedUserName.split("");
+    formattedUserName[0] = formattedUserName[0].toUpperCase();
+    formattedUserName = formattedUserName.join("");
+    return formattedUserName;
+}
+
+const parseBankAccount = (bankAccount: string) => {
+    const formattedBankAccount: string | string[] = removeSpaces(bankAccount.toUpperCase());
+    return splitToChunks(formattedBankAccount, 4).join(" ");
+}
+
 export const SettingsModal = (props: SettingsModalProps) => {
     const { state } = useContext(SettingsContext);
     const [username, setUserName] = useState(state.username);
@@ -18,8 +69,35 @@ export const SettingsModal = (props: SettingsModalProps) => {
     const [saveUserName] = useSaveUserName();
     const [saveBankAccount] = useSaveBankAccount();
     const [saveMobilePay] = useSaveMobilePay();
+    const [validBankAccount, isValidBankAccount] = useState(validateBankAccount(state.bankAccount));
+    const [validMobilePay, isValidMobilePay] = useState(validateMobilePay(state.mobilePay));
 
-
+    const formatAndSaveUserName = () => {
+        if (username === "") {
+            setUserName(state.username);
+            return;
+        }
+        setUserName(parseUserName(username));
+        saveUserName(parseUserName(username));
+    }
+    const formatAndSaveBankAccount = () => {
+        if (bankAccount === "") {
+            setBankAccount(state.bankAccount);
+            return;
+        }
+        isValidBankAccount(validateBankAccount(bankAccount));
+        setBankAccount(parseBankAccount(bankAccount));
+        saveBankAccount(parseBankAccount(bankAccount));
+    }
+    const formatAndSaveMobilePay = () => {
+        if (mobilePay === "") {
+            setMobilePay(state.mobilePay);
+            return;
+        }
+        isValidMobilePay(validateMobilePay(mobilePay));
+        setMobilePay(parseMobilePay(mobilePay));
+        saveMobilePay(parseMobilePay(mobilePay));
+    }
 
     return (<CustomModal setModal={props.setModal} title="Settings">
         <View style={styles.inputContainer}>
@@ -30,9 +108,7 @@ export const SettingsModal = (props: SettingsModalProps) => {
                 wrapperStyle={styles.inputWrapper}
                 defaultValue={username}
                 onChange={(e: NativeSyntheticEvent<TextInputFocusEventData>) => setUserName(e.nativeEvent.text)}
-                onBlur={() => {
-                    if (username) saveUserName(username);
-                }} />
+                onBlur={formatAndSaveUserName} />
         </View>
         <View style={styles.inputContainer}>
             <Text style={styles.labelText}>Bank account:</Text>
@@ -41,10 +117,8 @@ export const SettingsModal = (props: SettingsModalProps) => {
                 multiline={false}
                 defaultValue={bankAccount}
                 onChange={(e: NativeSyntheticEvent<TextInputFocusEventData>) => setBankAccount(e.nativeEvent.text)}
-                onBlur={() => {
-                    if (bankAccount) saveBankAccount(bankAccount);
-                }}
-                wrapperStyle={styles.inputWrapper} />
+                onBlur={formatAndSaveBankAccount}
+                wrapperStyle={[styles.inputWrapper, !validBankAccount && { borderColor: Colors.unPaidColor }]} />
         </View>
         <View style={styles.inputContainer}>
             <Text style={styles.labelText}>Mobile pay:</Text>
@@ -53,10 +127,8 @@ export const SettingsModal = (props: SettingsModalProps) => {
                 multiline={false}
                 defaultValue={mobilePay}
                 onChange={(e: NativeSyntheticEvent<TextInputFocusEventData>) => setMobilePay(e.nativeEvent.text)}
-                onBlur={() => {
-                    if (mobilePay) saveMobilePay(mobilePay);
-                }}
-                wrapperStyle={styles.inputWrapper} />
+                onBlur={formatAndSaveMobilePay}
+                wrapperStyle={[styles.inputWrapper, !validMobilePay && { borderColor: Colors.unPaidColor }]} />
         </View>
     </CustomModal>)
 };
